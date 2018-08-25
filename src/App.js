b0b3c4466f76b1b6fe678ce7ee1bcdba7a078aaa
zbarfield <zbarfield@knights.ucf.edu>
2018-08-25 19:45:21 -0400

    MY FIRST CONTAINER!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './containers/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-           </ul>
-         </div>
-       </nav>
+       <NavBar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
