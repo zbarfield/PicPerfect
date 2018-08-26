0e5a581bcf04441ddb0158e23e7bf3936c7bd507
zbarfield <zbarfield@knights.ucf.edu>
2018-08-26 10:06:28 -0400

    Side bar and text field at bottom

---
+++ src/components/PicToolBox.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PicToolBox extends Component {
+  render(){
+    return(
+      <React.Fragment>
+        <div class="collection">
+          <a href="#!" class="collection-item">Action</a>
+          <a href="#!" class="collection-item">Action</a>
+          <a href="#!" class="collection-item">Action</a>
+          <a href="#!" class="collection-item">Action</a>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PicToolBox
--- src/containers/PicEditor.js
+++ src/containers/PicEditor.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import PicViewer from './../components/PicViewer';
-import logo from '../logo.svg';
+import PicViewer from '../components/PicViewer';
+import PicToolBox from '../components/PicToolBox';
 
 class PicEditor extends Component {
   render(){
     return(
       <React.Fragment>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <PicViewer/>
-        </p>
-
+        <style>{'body { background-color: #263238; }'}</style>
+        <div class="row">
+          <div class="col s3">
+            <PicToolBox/>
+          </div>
+          <div class="col s6">
+            <PicViewer/>
+          </div>
+        </div>
+        <div class="row">
+        <textarea rows="4">
+            At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+        </textarea>
+        </div>
       </React.Fragment>
     );
   }
